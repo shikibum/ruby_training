614116ed490d342908fc4e5629a4734ccff14afc
Rina Mito <satoshikibu@gmail.com>
2018-03-06 22:43:58 +0900

    第13回まで

---
+++ ruby013.rb
@@ -0,0 +1,15 @@
+#if
+# 比較演算子 > < >= <= == != 〜と等しくない
+# 論理演算子 && (and) ||(or) !(not)
+
+score = gets.to_i
+
+# if score > 80
+#   puts "giant otter!"
+# elsif score > 60
+#   puts "smooth coated otter!"
+# else
+#   puts "kawauso"
+# end
+
+puts "Yamato!" if score >= 80 
