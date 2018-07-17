dfa1f28ceaa9b82ef4c648e8dcf86536ae3ac6b0
Rina Mito <satoshikibu@gmail.com>
2018-07-17 21:52:58 +0900

    ruby練習 ファイル出力

--- mytraining/003.rb
+++ mytraining/003.rb
@@ -1,3 +1,13 @@
-puts '文字を入力してください。'
+# puts '文字を入力してください。'
+# input = STDIN.gets
+# puts "Hello!" + input.chomp!
+
+puts 'ファイル名を入れてください'
 input = STDIN.gets
-puts "Hello!" + input.chomp!
+# require 'fileutils'
+# FileUtils.touch(input)
+
+open(input,"w") do |f|
+  f.puts("かわうそ")
+  f.puts("かわいい")
+end
