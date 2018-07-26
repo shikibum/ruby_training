d524be50cf5c45c79c53e95fb6dfc1767e1c477e
Rina Mito <satoshikibu@gmail.com>
2018-07-26 20:27:08 +0900

    ruby入門　文字列まで

---
+++ ruby_nyumon/001.rb
@@ -0,0 +1,25 @@
+# a = "かわうそ"
+# #a = "\U+FFE3\U+FFE3\U+FFE3\U+FFE3"
+# puts a.encode("UTF-8")
+
+# print "かわうそ"
+# print "Cute!"
+
+# print ("名前は #{'伊藤'} です\n")
+# print ("年齢は #{20 - 5} です\n")
+
+# name = "東京"
+# print ("出身は #{name} です\n")
+
+# str1 = "名前は"
+# str2 = "佐藤"
+# str3 = "です"
+# p (str = str1 + str2 + str3)
+
+p ("こんにちは" * 2)
+
+a = "Good!" * 10
+p (a)
+
+str = "Otter,"
+p str.concat("Cute!")
