cf623708cb932b3685f54aad68b075514ef8e60d
Rina Mito <satoshikibu@gmail.com>
2018-07-30 16:36:24 +0900

    Ruby練習 モジュール

---
+++ ruby_nyumon/012.rb
@@ -0,0 +1,47 @@
+# module AverageModule
+#   def average(a, b)
+#     result = (a + b) / 2
+#     result
+#   end
+# end
+
+# class Test
+#   include AverageModule
+
+#   def display(a, b)
+#     result = average(a, b)
+#     a.to_s + "と" + b.to_s + "の平均は" + result.to_s + "です"
+#   end
+# end
+
+# test = Test.new
+# puts test.display(10, 6)
+
+module ValueModule
+  def minValue(a, b)
+    if a < b
+      a
+    else
+      b
+    end
+  end
+
+  def maxValue(a, b)
+    if a > b
+      a
+    else
+      b
+    end
+  end
+
+  module_function :minValue
+  module_function :maxValue
+end
+
+# puts ValueModule.minValue(10, 8)
+# puts ValueModule.maxValue(10, 8)
+
+include ValueModule
+puts minValue(10, 8)
+puts maxValue(10, 8)
+
