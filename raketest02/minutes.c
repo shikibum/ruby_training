60e4b199b08fe15f9d65a09605f395fb7ea8e082
Rina Mito <satoshikibu@gmail.com>
2018-08-02 23:55:22 +0900

    rakeの基本 getsをfgetsに修正

--- raketest02/minutes.c
+++ raketest02/minutes.c
@@ -6,23 +6,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int  main( )
+int main()
 {
-   char    c[128];
-   int     m,h,m2;
+  char c[128];
+  int m, h, m2;
 
-    printf("\n**********************");
-    printf("\n*  時間の表示変換    *");
-    printf("\n* ○時間○分→○○分 *");
-    printf("\n**********************\n");
+  printf("\n**********************");
+  printf("\n*  時間の表示変換    *");
+  printf("\n* ○時間○分→○○分 *");
+  printf("\n**********************\n");
 
-    printf("\n時間： "); fflush(stdout);
-    gets(c); h=atoi(c);                 // 時入力，数値に変換
+  printf("\n時間： ");
+  fflush(stdout);
+  // gets(c); h=atoi(c);                 // 時入力，数値に変換
+  fgets(c, 128, stdin);
+  h = atoi(c); // 時入力，数値に変換
 
-    printf("\n分  ： "); fflush(stdout);
-    gets(c); m=atoi(c);                 // 分入力，数値に変換
+  printf("\n分  ： ");
+  fflush(stdout);
+  // gets(c); m=atoi(c);                 // 分入力，数値に変換
+  fgets(c, 128, stdin);
+  m = atoi(c); // 分入力，数値に変換
 
-    m2=h*60+m;
+  m2 = h * 60 + m;
 
-    printf("\n %d 時間 %d 分 → %d 分\n",h,m,m2);
+  printf("\n %d 時間 %d 分 → %d 分\n", h, m, m2);
 }
