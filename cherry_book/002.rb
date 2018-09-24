eda70f7c8b458af2880c478d8ae404fd353bfb67
shikibum <satoshikibu@gmail.com>
2018-09-24 23:52:31 +0900

    第2章 2.12.6まで

--- cherry_book/002.rb
+++ cherry_book/002.rb
@@ -28,3 +28,40 @@ end
 default_args(1, 2)
 default_args(1, 2, 3)
 default_args(1, 2, 3, 4)
+
+# システム日時や他のメソッドの戻り値をデフォルト値に指定する
+def foo(time = Time.now, message = bar)
+  puts "time: #{time}, message: #{message} "
+end
+
+def bar
+  'BAR'
+end
+foo
+
+# エイリアスメソッド 名前が違うだけで同じ
+'hello'.length
+'hello'.size
+
+# 参照の概念
+a = 'hello'
+b = 'hello'
+a.object_id #=> 70363124446160
+b.object_id #=> 70363124400000
+
+c = b
+
+def m(d)
+  d.object_id
+end
+m(c)
+
+# 渡された文字列を破壊的に大文字に変換するメソッドを定義
+def m!(d)
+  d.upcase!
+end
+m!(c)
+
+# b,cいずれも大文字になる
+b #=> 'HELLO'
+c #=> 'HELLO'
