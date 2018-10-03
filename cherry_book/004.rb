849923195660ae74540d1016e97e412a8c0376bc
shikibum <satoshikibu@gmail.com>
2018-10-03 23:48:38 +0900

    4.3.4まで

--- cherry_book/004.rb
+++ cherry_book/004.rb
@@ -16,3 +16,27 @@ a.delete_if do |n|
   n.odd?
 end
 a #=> [2, 2]
+
+numbers = [1, 2, 3, 4]
+sum = 0
+numbers.each do |n|
+  sum += n
+end
+# ブロック引数の名前はなんでもよい
+
+numbers = [1, 2, 3, 4]
+sum = 0
+numbers.each do |n|
+  sum_value = n.even? ? n * 10 : n
+  sum += sum_value
+end
+sum #=> 64
+
+numbers = [1, 2, 3, 4]
+sum = 0
+sum_value = 100
+numbers.each do |sum_value|
+  sum += sum_value
+end
+sum #=>10
+# ブロック引数の名前をブロック大野にある変数の名前と一緒にするとブロック内ではブロック引数のの値が優先して参照される
