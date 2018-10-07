86f42804d5eab1f365128e5927fb93149a79ff35
shikibum <satoshikibu@gmail.com>
2018-10-07 12:03:34 +0900

    4.4.5まで

--- cherry_book/005.rb
+++ cherry_book/005.rb
@@ -11,4 +11,20 @@ numbers = [1, 2, 3, 4, 5, 6]
 even_numbers = numbers.select { |n| n.even? }
 even_numbers #=> [2, 4, 6]
 
+numbers = [1, 2, 3, 4, 5, 6]
+non_multiples_of_three = numbers.reject { |n| n % 3 == 0 }
+non_multiples_of_three #=> [1, 2, 4, 5]
 
+numbers = [1, 2, 3, 4, 5, 6]
+even_number = numbers.find { |n| n.even? }
+even_number #=> 2
+
+numbers = [1, 2, 3, 4]
+sum = numbers.inject(0) { |result, n| result + n}
+sum #=> 10
+
+['ruby', 'java', 'perl'].map { |s| s.upcase }
+['ruby', 'java', 'perl'].map(&:upcase)
+# 書き換えできる
+[1, 2, 3, 4, 5, 6].select { |n| n.odd? }
+[1, 2, 3, 4, 5, 6].select(&:odd?)
