f3ff18d1173de8d17b0da6459e9ecff41c6d73bc
shikibum <satoshikibu@gmail.com>
2018-10-21 19:00:23 +0900

    4.8.5

--- cherry_book/008.rb
+++ cherry_book/008.rb
@@ -22,3 +22,46 @@ fruits.each.with_index(1) { |fruit, i| puts "#{i}: #{fruit}" }
 #3: melon
 fruits.map.with_index(10) { |fruit, i| "#{i}: #{fruit}" }
 #=> ["10: apple", "11: orange", "12: melon"]
+
+dimentions = [
+  # [縦, 横]
+  [10, 20],
+  [30, 40],
+  [50, 60]
+]
+areas = []
+dimentions.each do |dimention|
+  length = dimentions[0]
+  width = dimention[1]
+  areas << length * width
+end
+#=> [[10, 20], [30, 40], [50, 60]]
+
+#ブロック引数を2個
+dimentions.each do |length, width|
+  areas << length * width
+end
+#=> [[10, 20], [30, 40], [50, 60]]
+
+#ブロック引数雨を()で囲んで、配列の要素を別々の引数として受け取る
+dimentions.each_with_index do |(length, width), i|
+  puts "length: #{length}, wodth: #{width}, i:#{i}"
+end
+#length: 10, wodth: 20, i:0
+#length: 30, wodth: 40, i:1
+#length: 50, wodth: 60, i:2
+
+#ブロックローカル変数
+numbers = [1, 2, 3, 4]
+sum = 0
+numbers.each do |n; sum|
+  sum = 10
+  sum += n
+  p sum
+end
+#11
+#12
+#13
+#14
+#=> [1, 2, 3, 4]
+sum #=> 0 
