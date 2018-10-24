e3e257e17b5bc2309461606c4c08c6c2045c5ef2
shikibum <satoshikibu@gmail.com>
2018-10-24 20:42:02 +0900

    4.8.7

--- cherry_book/008.rb
+++ cherry_book/008.rb
@@ -65,3 +65,26 @@ end
 #14
 #=> [1, 2, 3, 4]
 sum #=> 0 
+
+File.open("./sample.txt", "w") do |file|
+  file.puts("1行目のテキストです。")
+  file.puts("2行目のテキストです。")
+  file.puts("3行目のテキストです。")
+end
+
+a = [1, 2, 3]
+
+a.delete(100) #=> nil
+a.delete(100) do
+  'NG'
+end
+#=> "NG"
+
+a.delete 100 do
+  'NG'
+end
+#=> "NG"
+a.delete 100 { 'NG' }
+#=> SyntaxError ((irb):105: syntax error, unexpected '{', expecting end-of-inpu
+a.delete (100) { 'NG' }
+#=> "NG"
