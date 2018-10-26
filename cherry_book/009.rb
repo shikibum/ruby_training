8b95baf150c364e09c4ed60c41b192fb14cefebb
shikibum <satoshikibu@gmail.com>
2018-10-26 23:58:06 +0900

    4.9.4

--- cherry_book/009.rb
+++ cherry_book/009.rb
@@ -21,3 +21,29 @@ a #=> [1, 3, 5, 7, 9]
 a = []
 10.step(1, -2)  { |n| a << n }
 a #=> [10, 8, 6, 4, 2]
+
+a = []
+while  a.size < 5 do a << 1 end
+a #=> [1, 1, 1, 1, 1]
+
+a = []
+a << 1 while a.size < 5
+a #=> [1, 1, 1, 1, 1]
+
+a = []
+while false
+  a << 1
+end
+a #=> []
+
+a = []
+begin
+  a << 1
+end while false
+a #=> [1]
+
+a = [10 , 20 , 30 ,40, 50]
+until a.size <= 3
+  a.delete_at(-1)
+end
+a #=>=> [10, 20, 30]
