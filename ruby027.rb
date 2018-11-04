ee336badc0700eb778957d42f327593ec69ab621
shikibum <satoshikibu@gmail.com>
2018-11-04 10:01:12 +0900

    send

--- ruby027.rb
+++ ruby027.rb
@@ -1,8 +1,20 @@
-p ARGV
-if ARGV[1] == "upcase" 
-  p ARGV[0].upcase
-elsif ARGV[1] == "downcase"
-  p ARGV[0].downcase
-else
-  p ARGV[0]
-end 
+# p ARGV
+# if ARGV[1] == "upcase" 
+#   p ARGV[0].upcase
+# elsif ARGV[1] == "downcase"
+#   p ARGV[0].downcase
+# else
+#   p ARGV[0]
+# end 
+
+# class Otter
+#   def upcase(n)
+#     puts n.upcase
+#   end
+  
+#  def downcase(n)
+#   puts n.downcase
+#  end
+# end
+
+puts ARGV[0].send(ARGV[1])
