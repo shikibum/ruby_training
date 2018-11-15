e7cea60c204465bd0a84a11965fbccc50e5aa496
shikibum <satoshikibu@gmail.com>
2018-11-15 22:03:18 +0900

    4.10.2

--- cherry_book/010.rb
+++ cherry_book/010.rb
@@ -42,3 +42,28 @@ end
 # orange, 2 
 # orange, 1 
 # orange, 3 
+
+fruits = ['apple', 'melon' , 'orange']
+numbers = [1, 2, 3]
+catch :done do
+  fruits.shuffle.each do |fruit|
+    numbers.shuffle.each do |n|
+      puts "#{fruit}, #{n}"
+      if fruit == 'orange' && n == 3
+        throw :done
+      end
+    end
+  end
+end
+
+ret =
+  catch :done do
+    throw :done
+end
+ret
+
+ret =
+  catch :done do
+    throw :done, 123
+  end
+ret
