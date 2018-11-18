528eaa791883e81b2abbacac1adf3d02f6dff148
shikibum <satoshikibu@gmail.com>
2018-11-18 10:56:04 +0900

    5.2.3まで

--- cherry_book/011.rb
+++ cherry_book/011.rb
@@ -15,3 +15,41 @@ currencies[:japan] = '円'
 currencies[:india] # => "rupee"
 currencies[:barzil] # => nil
 
+currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
+currencies.each do |key, value|
+  puts "#{key} : #{value}"
+end
+# japan : yen
+# us : dollar
+# india : rupee
+
+currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
+currencies.each do |key_value|
+  key = key_value[0]
+  value = key_value[1]
+  puts "#{key} : #{value}"
+end
+# japan : yen
+# us : dollar
+# india : rupee
+
+a = { x: 1, y: 2, z: 3}
+b = { x: 1, y: 2, z: 3}
+a == b # => true
+c = { z: 3, y: 2, x: 1 }
+a == c # => true
+d = { x: 10, y: 2, z: 3}
+a == d # => false
+
+{}.size # => 0
+{ x: 1, y: 2, z: 3}.size # => 3
+
+currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
+currencies.delete(:japan) #=> "yen"
+currencies #=> {:us=>"dollar", :india=>"rupee"}
+
+currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
+currencies.delete(:italy) #=> nil
+currencies.delete(:italy){|key| "Not found: #{key}"}
+#=> "Not found: italy"
+
