e41a2677ccdfb5b1e22c692a0a5daf9fa9cbcb89
shikibum <satoshikibu@gmail.com>
2018-11-25 17:46:33 +0900

    5.6.8

--- cherry_book/013.rb
+++ cherry_book/013.rb
@@ -8,8 +8,59 @@ h = { us: 'doller', india: 'rupee'}
 { japan: 'yen', **h } #=> {:japan=>"yen", :us=>"doller", :india=>"rupee"}
 { japan: 'yen' }.merge(h) #=> {:japan=>"yen", :us=>"doller", :india=>"rupee"}
 
-def buy_burger(menu, option = {})
+def buy_burger(menu, options = {})
   drink = options[:drink]
-  potato = options{:potato}
+  potato = options[:potato]
 end
 buy_burger('cheese', drink: true, potato: true)
+
+def buy_burger(menu, drink: true, potato: false, **other)
+  puts other
+end
+
+buy_burger('fish', drink: true, potato: false, salad: true, chicken: false)
+# =>{:salad=>true, :chicken=>false}
+
+buy_burger('fish', 'drink' => true, 'potato' => false) #=> {drink' => true, 'potato' => false}
+
+def buy_burger(options = {}, menu)
+  puts options
+end
+buy_burger({'drink' => true, 'potato' => false}, 'fish')
+
+def buy_burger(menu, options = {})
+  puts options
+end
+# 第2引数以降にハッシュが来る場合は（）を省略してもエラーにならない
+buy_burger 'fish',{'drink' => true, 'potato' => false}
+# ハッシュが最後の引数なので{}を省略できる
+buy_burger 'fish','drink' => true, 'potato' => false
+
+currencies = { japan: 'yen', us: 'dollar', india: 'rupee' }
+currencies.to_a # => [[:japan, "yen"], [:us, "dollar"], [:india, "rupee"]]
+array = [[:japan, "yen"], [:us, "dollar"], [:india, "rupee"]]
+array.to_h #=> {:japan=>"yen", :us=>"dollar", :india=>"rupee"}
+
+h = Hash.new('hello')
+a = h[:foo] #=> "hello"
+b = h[:bar] #=> "hello"
+a.equal?(b) #=> true
+a.upcase!
+a #=> "HELLO"
+b #=> "HELLO"
+h #=> {}
+
+h = Hash.new { 'hello' }
+a = h[:foo] #=> "hello"
+b = h[:bar] #=> "hello"
+a.equal?(b) #=> false
+a.upcase!
+a #=> "HELLO"
+b #=> "hello"
+
+
+
+
+
+
+
